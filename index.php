f20190c98fc13adf8e0602902db6a7018e99aa46
inleidingJava <peyman_mah6@yahoo.com>
2018-03-15 11:42:06 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peyma
+ * Date: 3/15/2018
+ * Time: 9:42 AM
+ */
+echo "jo go jo";
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peyma
+ * Date: 3/15/2018
+ * Time: 11:40 AM
+ */
