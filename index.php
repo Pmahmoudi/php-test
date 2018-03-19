9836f67536262e17369a441bfd085913177cc9eb
inleidingJava <peyman_mah6@yahoo.com>
2018-03-19 10:59:22 +0100

    first commit

--- index.php
+++ index.php
@@ -5,4 +5,28 @@
  * Date: 3/15/2018
  * Time: 9:42 AM
  */
-echo "jo go jo";
+$a = "Ik heb niet gefaald.";
+$b = " die niet werken.''";
+$c = 1000 ;
+
+echo $a." Ik heb alleen ".$c." manieren gevonden".$b."<br>";
+
+$d = " die geen fout";
+$e = "en";
+$f = " niet. ";
+
+echo "Mensen".$d.$e." maken, "."werken".$f."<br>";
+
+
+$g = "ry";
+$h =", but";
+$i = "fail";
+
+echo "T".$g." and ".$i.$h." never ".$i." to t".$g."."."<br>";
+
+
+$j = "omgaan";
+$k = "met";
+$l = "teleu";
+
+echo "De cursus "."''$j $k ".$l."restellingen''"." kan vanavound helaas niet doorgaan.";
